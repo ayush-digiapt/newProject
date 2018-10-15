005b6ccd70d52d36b6dafcf6674069fcc7061ff8
ayush-digiapt <ayush.ranka@digiapt.com>
2018-10-15 12:04:11 +0530

    API

--- controllers/auth.js
+++ controllers/auth.js
@@ -9,7 +9,7 @@ exports.login = function(req,res) {
     console.log("in login");
     console.log("body: ",req.body);
     var connection = db.getDbConnection();
-    var queryStatement = "select * from admins where email='"+req.body.email+"' and password='"+req.body.password+"'";
+    var queryStatement = "select email from admins where email='"+req.body.email+"' ";
 	connection.query(queryStatement,function(err,result){
 		if(err) {
 			console.log("error: ",err);
@@ -19,13 +19,33 @@ exports.login = function(req,res) {
 		} else {
            // console.log("success: ",result);
             if(result.length>0){
-                res.status(200).send(" successfully login result");
-                console.log("hii login successfull");
-            } else {
-                res.status(401).send("No user found");
-                console.log("ohhh login not successfull");
+                var queryStatement = "select * from admins where email='"+req.body.email+"' and password='"+req.body.password+"'";
+                connection.query(queryStatement,function(err,result){
+                    if(err) {
+                        console.log("error: ",err);
+                        res.status(400).send(err);
+                       
+                                
+                    } else {
+                       // console.log("success: ",result);
+                        if(result.length>0){
+                            res.status(200).send(" successfully login result");
+                            console.log("hii login successfull");
+                        } else {
+                            res.status(401).send("password is wrong");
+                            console.log("password is wrong");
+                        }
+                // res.status(200).send(" successfully login result");
+                // console.log("hii login successfull");
+            } 
+        });
+        }else {
+                res.status(401).send("Email id is not valid");
+                console.log("Email id is not valid");
             }
 			
-		}
-	});
+        
+	
+    }
+});
 }
